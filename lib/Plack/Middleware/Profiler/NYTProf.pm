e80e8e83c438f62377d6ef9afd0262a7980f96dd
Dann <techmemo@gmail.com>
2009-11-29 23:12:54 +0900

    initial commit

---
+++ lib/Plack/Middleware/Profiler/NYTProf.pm
@@ -0,0 +1,47 @@
+package Plack::Middleware::Profiler::NYTProf;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Profiler::NYTProf -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Profiler::NYTProf;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Profiler::NYTProf is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+  http://github.com/dann/
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
