423d6849530a64b814f59d9b7b93313172a85966
Dann <techmemo@gmail.com>
2013-02-08 23:44:43 +0900

    Bump version

--- lib/Plack/Middleware/Profiler/NYTProf.pm
+++ lib/Plack/Middleware/Profiler/NYTProf.pm
@@ -2,7 +2,7 @@ package Plack::Middleware::Profiler::NYTProf;
 use strict;
 use warnings;
 use parent qw(Plack::Middleware);
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Plack::Util::Accessor qw(
     enable_profile
